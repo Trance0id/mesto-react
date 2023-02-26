f3ce97169be8456ad59fee0cd66074b9baec5468
Dmitriy Vasnev <trance0id@yandex.ru>
2023-02-26 15:07:52 +0300

    api: added UserInfo query

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import api from '../utils/Api.js';
 
 function Main({ onEditAvatar, onEditProfile, onAddPlace }) {
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+
+  React.useEffect(() => {
+    api.getUserInfo().then(res => {
+      setUserName(res.name);
+      setUserDescription(res.about);
+      setUserAvatar(res.avatar);
+    })
+      .catch(err => {
+        console.log(err);
+        alert(`Не удалось получить ответ от сервера. \n${err}`);
+      });
+  }, []);
 
   return (
     <main className="content">
       <section className="profile" aria-label="Данные профиля">
         <div className="profile__avatar-wrapper">
-          <img src="#" alt="Аватар пользователя" className="profile__avatar" />
+          <img src={userAvatar} alt="Аватар пользователя" className="profile__avatar" />
           <button type="button" className="profile__avatar-button" aria-label="Изменить аватар" onClick={onEditAvatar}></button>
         </div>
         <div className="profile__info">
-          <h1 className="profile__name"></h1>
+          <h1 className="profile__name">{userName}</h1>
           <button type="button" className="button profile__edit-button" aria-label="Редактировать" onClick={onEditProfile}></button>
-          <p className="profile__description"></p>
+          <p className="profile__description">{userDescription}</p>
         </div>
         <button type="button" className="button profile__add-button" aria-label="Добавить" onClick={onAddPlace}></button>
       </section>
